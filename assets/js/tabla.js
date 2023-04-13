9812d52e3662f180f9769059a9f4f9d5d2c78947
Felipe-Blest <102250682+Felipe-Blest@users.noreply.github.com>
2023-04-12 22:51:39 -0400

    Contacto responsivo + intento responsivo tabla

--- assets/js/tabla.js
+++ assets/js/tabla.js
@@ -114,6 +114,19 @@ var data = [
 
 $(document).ready(function () {
    $('#insertInfo').DataTable({
+      responsive: {
+         breakpoints: [
+            { name: 'bigdesktop', width: Infinity },
+            { name: 'meddesktop', width: 1480 },
+            { name: 'smalldesktop', width: 1280 },
+            { name: 'medium', width: 1188 },
+            { name: 'tabletl', width: 1024 },
+            { name: 'btwtabllandp', width: 848 },
+            { name: 'tabletp', width: 768 },
+            { name: 'mobilel', width: 480 },
+            { name: 'mobilep', width: 320 }
+         ]
+      },
       language: {
          url: 'https://cdn.datatables.net/plug-ins/1.10.25/i18n/Spanish.json'
       },
